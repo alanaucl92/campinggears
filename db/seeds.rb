4b60bb8816d8f08cdeb5e8f75acb76f01e1dd320
Alan Au <alanaucl92@gmail.com>
2023-03-15 16:48:45 -0300

    updatedreservelogic

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 require "open-uri"
 
 seeduser = User.create!(email: "alanaucl@gmail.com", password: "123123")
+seeduser2 = User.create!(email: "mandy@gmail.com", password: "123123")
 file = URI.open("https://www.rei.com/media/2f9c8c47-299b-4a4c-aa05-b10fbf303691.jpg")
 gear = Gear.new(name: "Wawona 6 tent", brand: "The North Face", category: "Tent", price: 8.5, user: seeduser)
 gear.photo.attach(io: file, filename: "Wawona_6.png", content_type: "image/png")
@@ -19,6 +20,10 @@ gear.save
 file = URI.open("https://www.rei.com/media/17d0f0fe-15d4-47bc-9113-aaf24eee0509.jpg")
 gear = Gear.new(name: "Half Dome SL 2+ Tent with Footprint", brand: "REI Co-op", category: "Tent", price: 6, user: seeduser)
 gear.photo.attach(io: file, filename: "DomeSL.png", content_type: "image/png")
+gear.save
+file = URI.open("https://www.thesportinglodge.co.uk/wp-content/uploads/2021/03/Snow-Peak-Taut-Line-Hitch-T-Shirt-White-18982-1.jpg")
+gear = Gear.new(name: "Snowpeak T-shirt", brand: "Snowpeak", category: "Clothes", price: 2, user: seeduser2)
+gear.photo.attach(io: file, filename: "SPTshirt.png", content_type: "image/png")
 gear.save
 # seeduser = User.create!(email: "alanaucl@gmail.com", password: "123123")
 # Gear.create!(name: "Good sleeping", brand: "Reecho", category: "Sleeping Bag", price: 6.9 ,user: seeduser)
